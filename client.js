1098444687d5f03c35e168e6a81c9bf35418f27e
Tracy <793889968@qq.com>
2020-08-28 07:15:02 +0000

    fit safari

--- client.js
+++ client.js
@@ -1,4 +1,4 @@
-window.onload = () => {
+window.onload = function() {
     const arena = document.getElementById('arena');
     const ctx = arena.getContext("2d");
     const socket = io();
@@ -24,28 +24,27 @@ window.onload = () => {
     const fps = 60;
 
     // status of each turn
-    let timer;
-    let clients = [];
-    let currentStatus = status.idle;
-    let totalClientNumber = 0;
-    let tick = 0;
-    let lastTick = -1;
-    let actions = [];
-    let lastTs;
-    let req;
+    var timer;
+    var clients = [];
+    var currentStatus = status.idle;
+    var totalClientNumber = 0;
+    var tick = 0;
+    var lastTick = -1;
+    var lastTs;
+    var req;
 
-    let _matching = document.getElementById("matching");
-    let _matchingButton = document.getElementById("matching-button");
-    let _matchingInfo = document.getElementById("matching-info");
-    let _console = document.getElementById("_console");
+    var _matching = document.getElementById("matching");
+    var _matchingButton = document.getElementById("matching-button");
+    var _matchingInfo = document.getElementById("matching-info");
+    var _console = document.getElementById("_console");
     
-    let output = (message) => {
+    var output = function(message) {
         _console.innerHTML += message + "<br>";
     }
 
-    let draw = () => {
+    var draw = function() {
         ctx.clearRect(0, 0, arena.width, arena.height);
-        clients.forEach((client, index) => {
+        clients.forEach(function(client, index) {
             ctx.beginPath();
             ctx.arc(client.position.x, client.position.y, 20, 0, Math.PI * 2, true);
             ctx.closePath();
@@ -55,7 +54,7 @@ window.onload = () => {
         req = window.requestAnimationFrame(draw);
     }
 
-    _matchingButton.addEventListener('click', () => {
+    _matchingButton.addEventListener('click', function() {
         if (socket.connected) {
             _matchingInfo.innerHTML = "waiting...(0/" + totalClientNumber + ")";
             currentStatus = status.pending;
@@ -66,33 +65,33 @@ window.onload = () => {
         }
     });
 
-    socket.on('connected', (message) => {
+    socket.on('connected', function(message) {
         totalClientNumber = message.totalClientNumber;
         output(message.info);
     });
 
-    socket.on('connectFailed', (message) => {
+    socket.on('connectFailed', function(message) {
         output(message.info);
     });
 
-    socket.on('clientJoined', (message) => {
+    socket.on('clientJoined', function(message) {
         _matchingInfo.innerHTML = "waiting...(" + message.currentClientNumber + "/" + message.totalClientNumber + ")";
         output(message.info);
     });
 
-    socket.on('start', (message) => {
+    socket.on('start', function(message) {
         clearInterval(timer);
         bindInputHandler();
         currentStatus = status.inProgress;
         clients = message.clients;
-        setTimeout(() => {
+        setTimeout(function() {
             resetPanel();
-            timer = setInterval(() => {
+            timer = setInterval(function() {
                 if (lastTick !== tick) {
-                    let nowTs = +new Date();
-                    let delta = (nowTs - (lastTs || nowTs)) / 1000.0;
+                    var nowTs = +new Date();
+                    var delta = (nowTs - (lastTs || nowTs)) / 1000.0;
                     lastTs = nowTs;
-                    let action;
+                    var action;
                     if (key.left) {
                         action = { "delta": delta, "type": actionTypes.moveToLeft }
                     } else if (key.right) {
@@ -116,11 +115,11 @@ window.onload = () => {
         }, 1000);
     });
     
-    socket.on('update', (message) => {
-        message.actions.forEach((client) => {
-            clients.forEach((_client) => {
+    socket.on('update', function(message) {
+        message.actions.forEach(function(client) {
+            clients.forEach(function(_client) {
                 if (_client.id === client.id) {
-                    let offset = client.action.delta * speed;
+                    var offset = client.action.delta * speed;
                     if (client.action.type === actionTypes.moveToLeft) {
                         _client.position.x -= offset;
                     } else if (client.action.type === actionTypes.moveToRight) {
@@ -136,14 +135,14 @@ window.onload = () => {
         tick++;
     });
 
-    socket.on('clientLeft', (message) => {
+    socket.on('clientLeft', function(message) {
         if (currentStatus === status.pending) {
             _matchingInfo.innerHTML = "waiting...(" + message.currentClientNumber + "/" + message.totalClientNumber + ")";
         }
         output(message.info);
     });
 
-    socket.on('roomClosed', (message) => {
+    socket.on('roomClosed', function(message) {
         ctx.clearRect(0, 0, arena.width, arena.height);
         clearInterval(timer);
         window.cancelAnimationFrame(req);
@@ -153,12 +152,13 @@ window.onload = () => {
         output(message.info);
     });
 
-    socket.on('matchingFailed', (message) => {
+    socket.on('matchingFailed', function(message) {
         output(message.info);
     });
 
-    let bindInputHandler = () => {
-        let keyHandler = function(event) {
+    var bindInputHandler = function() {
+        var keyHandler = function(event) {
+            event.preventDefault();
             if (event.keyCode === 37) {
                 key.left = (event.type === "keydown");
             } else if (event.keyCode === 38) {
@@ -173,23 +173,22 @@ window.onload = () => {
         document.body.onkeyup = keyHandler;
     }
 
-    let unbindInputHandler = () => {
+    var unbindInputHandler = function() {
         document.body.onkeydown = null;
         document.body.onkeyup = null;
     }
 
-    let resetStatus = () => {
+    var resetStatus = function() {
         clients = [];
         currentStatus = status.idle;
         timer = undefined;
         totalClientNumber = 0;
         tick = 0;
         lastTick = -1;
-        actions = [];
         lastTs = undefined;
     }
 
-    let resetPanel = () => {
+    var resetPanel = function() {
         if (currentStatus === status.inProgress) {
             _matching.style.display = 'none';
         } else {
